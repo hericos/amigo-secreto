11fd69536d0e86adc303265b22ade10e21dfd84f
Herico Araujo <hericos@gmail.com>
2025-11-19 17:32:28 -0300

    Atualizar o config.js

--- config.js
+++ config.js
@@ -1,3 +1,2 @@
-// Valor padrão apenas para desenvolvimento local.
-// No container, este arquivo será gerado dinamicamente pelo entrypoint.
-window.SORTUDO = "Fulano de Tal";
+window.SORTUDO = "Fulano";
+window.PARTICIPANTES = ["Fulano", "Beltrano", "Ciclano"];
